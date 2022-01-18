f4cd206b5ea94d48d4c0a7869e2fd3d484a4dcf5
Laura Mejía Celis <lauram2celis@gmail.com>
2022-01-18 09:33:36 -0500

    Conditional excercise

--- 02conditionals-exercises.js
+++ 02conditionals-exercises.js
@@ -3,10 +3,18 @@
   se puede pensionar si:
   * Hombre 60 años o más y 2000 semanas
   * Mujer 57 años o más y 1500 semanas
- 
+
 */
 var person = {
   name: 'lauris',
-  age: 33,
-  weeks: 1200
+  age: 58,
+  weeks: 1600,
+  gender: "female"
 }
+
+if ((person.gender === "male" && person.age >= 60 && person.weeks >= 1200) || (person.gender === "female" && person.age >= 57 && person.weeks >= 1500)) {
+  console.log("Se puede jubilar");
+} else {
+  console.log("NO se puede jubilar");
+} 
+
