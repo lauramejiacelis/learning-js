7b27c3106c755f572a2ab94c6435f6f7a4a6043d
Laura Mejía Celis <lauram2celis@gmail.com>
2022-01-25 14:28:16 -0500

    homework

--- 02conditionals-exercises.js
+++ 02conditionals-exercises.js
@@ -3,7 +3,6 @@
   se puede pensionar si:
   * Hombre 60 años o más y 2000 semanas
   * Mujer 57 años o más y 1500 semanas
-
 */
 var person = {
   name: 'lauris',
--- 04homework.js
+++ 04homework.js
@@ -6,12 +6,54 @@
  * anitalavalatina
  */
 
-function isPalindrom(word) {}
+function isPalindrom(word) {
+
+    var wordOrder = "";
+    var wordReverse = "";
+    for (let i = 0; i < word.length; i++) {
+        var y = word.length - 1;
+        wordOrder = wordOrder + word.charAt(i);
+        wordReverse = wordReverse + word.charAt(y-i);
+    }
+    console.log(wordOrder);
+    console.log(wordReverse);
+    if (wordOrder === wordReverse) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 console.log(isPalindrom("anilina")); // (arr.length - 1) / 2 
 console.log(isPalindrom("arbol"));
 console.log(isPalindrom("anitalavalatina"));
 console.log(isPalindrom("anna")); // arr.length / 2
+
+function isPalindrom2(word) {
+
+    var wordOrder = "";
+    var wordReverse = "";
+    for (let i = 0; i < word.length; i++) {
+        var y = word.length - 1;
+        
+        if (console.log(word.charAt(i)) === console.log(word.charAt(y-i))) {
+            
+            //return true;
+        }
+    }
+    console.log(wordOrder);
+    console.log(wordReverse);
+    if (wordOrder === wordReverse) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isPalindrom2("anilina")); // (arr.length - 1) / 2 
+console.log(isPalindrom2("arbol"));
+console.log(isPalindrom2("anitalavalatina"));
+console.log(isPalindrom2("anna")); // arr.length / 2
 
 /*
  * y = arr.length - 1
@@ -29,8 +71,8 @@ console.log(isPalindrom("anna")); // arr.length / 2
 
 /*
 1. inputs (word) y outputs (true/false)
-2. recorrerlo todo?  no
-3. operador? 
-4. estrategía? 
-5. resultado?  
+2. recorrerlo todo?  no, se recorre hasta la mitad
+3. operador? ===
+4. estrategía? comparar la primera y la última letra hasta llegar al medio
+5. resultado?  (true/false)
 */
