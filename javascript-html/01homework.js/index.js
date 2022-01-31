b68a153eac15dddc87c5ede8487be6f794862a76
Laura Mejía Celis <lauram2celis@gmail.com>
2022-01-31 17:32:21 -0500

    homework js and html

--- javascript-html/01homework.js/index.js
+++ javascript-html/01homework.js/index.js
@@ -2,3 +2,18 @@
  * Crear un cuadradito (puede ser un div), que cuando le de click
  * cambie de alto, ancho y tambien el background-color
  */
+
+var square = document.getElementById("square");
+var button = document.getElementById("button");
+
+var flag = false;
+var count = 0;
+button.addEventListener("click", function (event) {
+    square.classList = "";
+    square.classList.add(flag ? "square1" : "square2");
+    flag = !flag;
+    count++;
+    console.log(count);
+})
+
+console.log(button);
