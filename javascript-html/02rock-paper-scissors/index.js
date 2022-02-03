911f32995f35e6072027705a98d46496dd5efa4e
David Sttivend <dsangel@gmail.com>
2022-02-02 20:22:17 -0500

    turn and move selection

--- javascript-html/02rock-paper-scissors/index.js
+++ javascript-html/02rock-paper-scissors/index.js
@@ -3,6 +3,13 @@ var addPlayer = document.getElementById("addPlayer");
 var inputMessage = document.getElementById("inputMessage");
 var setup = document.getElementById("setup");
 var board = document.getElementById("board");
+var roundIndicator = document.getElementById("roundIndicator");
+var turnIndicator = document.getElementById("turnIndicator");
+
+var round = 0;
+var turn = 0;
+
+var game = [];
 
 var players = [
   {
@@ -15,6 +22,18 @@ var players = [
     name: "",
     score: 0,
   },
+];
+
+var PLAY_OPTIONS = [
+  {
+    name: "R",
+    beats: "S",
+  },
+  {
+    name: "P",
+    beats: "R",
+  },
+  { name: "S", beats: "P" },
 ];
 
 var step = 0;
@@ -34,7 +53,9 @@ addPlayer.addEventListener("click", function () {
 });
 
 function prepareBoard() {
+  var roundStatus = document.getElementById("roundStatus");
   board.classList.remove("hide");
+  roundStatus.classList.remove("hide");
 
   players.forEach(function (player) {
     var playerContent = document.createElement("div");
@@ -46,18 +67,57 @@ function prepareBoard() {
     var playerName = document.createElement("h1");
     playerName.innerText = player.name;
 
-    var playerRoundIndicator = document.createElement("input");
-    playerRoundIndicator.type = "radio";
-    playerRoundIndicator.name = "indicator";
+    var playerScore = document.createElement("span");
 
-    if (player.id === "player1") {
-      playerRoundIndicator.checked = true;
-    }
+    playerScore.innerText = 0;
+
+    var cardsContainer = document.createElement("div");
+    cardsContainer.classList.add("card-container");
+
+    PLAY_OPTIONS.forEach(function (option) {
+      var card = document.createElement("div");
+      card.classList.add("card");
+      card.innerText = option.name;
+
+      card.addEventListener("click", function () {
+        // mire a que carta le dieron y vaya valide
+        var playerInTurn = players[turn];
+        if (player.id === playerInTurn.id) {
+          if (!game[round]) {
+            game[round] = {};
+          }
+          game[round][playerInTurn.id] = card.innerText;
+          turn = turn === 0 ? 1 : 0;
+          turnIndicator.innerText = turn + 1;
+
+          if (game[round].player1 && game[round].player2) {
+            var player1Option = PLAY_OPTIONS.find(function (option) {
+              return option.name === game[round].player1;
+            });
+            console.log(player1Option);
+            if (player1Option.beats === game[round].player2) {
+              console.log("Gana player 1");
+              // darle un punto al ganador
+              // aumentar la ronda
+              // validar si el juego termino
+            } else if (player1Option.name === game[round].player2) {
+              console.log("Empate");
+            } else {
+              console.log("Gana player 2");
+            }
+          }
+        } else {
+          console.log("No, wait for your turn");
+        }
+      });
+      cardsContainer.appendChild(card);
+    });
 
     playerContent.appendChild(playerStatus);
+    playerContent.appendChild(cardsContainer);
 
-    playerStatus.appendChild(playerRoundIndicator);
     playerStatus.appendChild(playerName);
+    playerStatus.appendChild(playerScore);
 
     board.appendChild(playerContent);
   });
