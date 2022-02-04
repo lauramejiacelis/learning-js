6cc3af5bd3eea807957fe5a0e71bf9521ed669ed
Laura Mejía Celis <lauram2celis@gmail.com>
2022-02-04 17:28:50 -0500

    homework lottery in process

--- javascript-html/03lottery/index.js
+++ javascript-html/03lottery/index.js
@@ -11,9 +11,11 @@
 var startButton = document.getElementById("startButton");
 var cardsContainer = document.getElementById("cardsContainer");
 
-//var CARDSQTY = 8;
+var round = 0;
 var cards = [];
+var plays = [];
 
+//* 1. Crear todas las cartas
 function createCards(CARDSQTY) {
     cards.length = CARDSQTY;
 
@@ -27,23 +29,37 @@ function createCards(CARDSQTY) {
 }
 
 startButton.addEventListener("click", function () {
-    createCards(8);
+    createCards(4);
     cards.forEach(function (element) {
         var card = document.createElement("div");
         card.classList.add("card");
         card.innerText = element;
-
+                
+        //* 2. Agregar el evento del click carta
+        cardsContainer.addEventListener("click", function (event) {
+            console.log(event.target.innerText);
+            var cardPlay = event.target;
+            console.log(`carta clickeada  ${cardPlay.innerText}`);
+            cardPlay.classList.remove("card");
+            cardPlay.classList.add("card-show");
+            plays.push(cardPlay.innerText);
+            console.log(`Jugadas ${plays}`);
+            if (plays.length ===2) {
+                if (plays [0] === plays[1]) {
+                    console.log("Las cartas son iguales");
+                } else {
+                    console.log("Las cartas son diferetes");
+                }
+            }
+        });
         cardsContainer.appendChild(card);
     });
+
     
 })
 
-cardsContainer.addEventListener("click", function (event) {
-    console.log(event.target.innerText);
-})
 
-
-
-//Necesito crear las cartas según un número par
-//y necesito escribir el texto pero de forma aleatoria de la cantidad dividido 2, la clase va a ser card
-//DAFSFD
+//* 3. Logica para seleccionar 2 cartas
+//* 4. Comparar las cartas
+// *    4.1 Iguales -> dejarlas volteadas y agregarlas a la la lista de cartas resueltas
+//*    4.2 Diferentes -> volver a ocultar y volver al paso 3
