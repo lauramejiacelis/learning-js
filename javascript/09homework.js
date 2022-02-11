29d1a652a94055a8f495e2ea876409f11dcee1d1
David Sttivend <dsangel@gmail.com>
2022-02-11 09:59:04 -0500

    Array Exercises

---
+++ javascript/09homework.js
@@ -0,0 +1,27 @@
+var pets = [
+  { name: "Amapola", procedures: [1] },
+  {
+    name: "Paco",
+    procedures: [1, 2, 3],
+  },
+  {
+    name: "Olivia",
+    procedures: [1, 3],
+  },
+];
+
+var procedure = {
+  1: { id: 1, name: "General revision", price: 20000 },
+  2: {
+    id: 2,
+    name: "prophylaxis",
+    price: 80000,
+  },
+  3: { id: 3, name: "grooming", price: 40000 },
+};
+
+/*
+    1. Total profit from the Vet
+    2. What procedure names Amapola paid for.
+    3. Which procedure was the most solicited
+  */
