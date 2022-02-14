b7b6044b531dab5913978a8371811c5db8dad26d
Laura Mejía Celis <lauram2celis@gmail.com>
2022-02-14 17:25:15 -0500

    homeworks in process

--- javascript/09homework.js
+++ javascript/09homework.js
@@ -25,3 +25,47 @@ var procedure = {
     2. What procedure names Amapola paid for.
     3. Which procedure was the most solicited
   */
+
+function getProp(prop) {
+  return function (obj) {
+    return obj[prop];
+  };
+}
+var getName = getProp("name");
+
+function getFromPet(obj) {
+  return function (prop) {
+    return obj[prop];
+  };
+}
+
+var amaProcedures = pets.find(function (pet) {
+  if (pet.name === "Amapola") {
+    //console.log(`Pet procedure: ${pet.procedures}`)
+    return pet.procedures;
+  }
+})
+//console.log(amaProcedures)
+//console.log(procedure[amaProcedures])
+//console.log(procedure[amaProcedures.procedures].name)
+
+var solicited = pets.map(function (pet) {
+  return pet.procedures;
+})
+solicited = solicited.reduce((acc, val) => acc.concat(val), []) //aplané la lista
+//console.log(solicited);
+const mostSolicited = ar => ar.reduce((acum, el, i, ar) => {
+  const count=ar.filter(e => e==el).length;
+  return count > acum[1] ? [el] : acum;
+}, ["", 0]
+) //encontrando el más solicitado
+//console.log(mostSolicited(solicited))
+
+
+var getFromPet = getFromPet(amaProcedures);
+console.log(`${getName(amaProcedures)} paid for Procedure: ${getName(procedure[getFromPet("procedures")])}`)
+
+console.log(`The most solicited procedure was: ${getName(procedure[mostSolicited(solicited)])}`);
+
+
+
