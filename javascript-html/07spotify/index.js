96f6c52aa74b5e9fd040eec65270896f079189f8
Laura Mejía Celis <lauram2celis@gmail.com>
2022-03-03 16:16:08 -0500

    just starting homework spotify

---
+++ javascript-html/07spotify/index.js
@@ -0,0 +1,22 @@
+//Buscador de artistas
+//Me pintas el nombre del artista y una foto
+
+var artist = document.getElementById("artist");
+var search = document.getElementById("search");
+
+
+
+/*
+fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${userAccessToken}`
+  }
+})
+.then(response => response.json())
+.then(({beats}) => {
+  beats.forEach((beat, index) => {
+    console.log(`Beat ${index} starts at ${beat.start}`);
+  })
+})
+*/
