5cda3974b53885ee81c38e6abea909aec0fa0fce
Laura Mejía Celis <lauram2celis@gmail.com>
2022-03-06 19:06:19 -0500

    spotify homework

--- javascript-html/07spotify/index.js
+++ javascript-html/07spotify/index.js
@@ -1,22 +1,44 @@
 //Buscador de artistas
 //Me pintas el nombre del artista y una foto
 
-var artist = document.getElementById("artist");
+var getArtist = document.getElementById("getArtist");
 var search = document.getElementById("search");
+var artistList = document.getElementById("artistList");
 
+const type = 'artist';
+const token =
+  'BQA6jpwA3flAXXFapw9n7uy_IQHUuIj9rndU3Jrp_tSwbnJpVRtlWZAi68i8Z4gFoXd86TcmkcggR6hmr0xr5_yGgdIvQd1j64opnXqFZytLPj8WFSqvb_YDAjg9qjsJZ1R4vsC7dB2UVStlGjg9ihr8GcLX8ezOyQQ';
 
-
-/*
-fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
-  method: "GET",
+search.addEventListener("click", function () {
+  console.log(`Artist ${getArtist.value}`)
+  fetch(`https://api.spotify.com/v1/search?q=${getArtist.value}&type=${type}`, {
   headers: {
-    Authorization: `Bearer ${userAccessToken}`
-  }
+    Authorization: `Bearer ${token}`,
+  },
 })
-.then(response => response.json())
-.then(({beats}) => {
-  beats.forEach((beat, index) => {
-    console.log(`Beat ${index} starts at ${beat.start}`);
+  .then((res) => res.json())
+  .then((artist) => artist.artists)
+  .then(({items}) => {
+    items.forEach(element => {
+      console.log(element.name)
+      var artistResult = document.createElement("div");
+      artistResult.classList.add("artist-results");
+      var image = document.createElement('img');
+      var url = "";
+      if (element.images != 0) {
+        url = element.images[1].url;
+        image.src = `${url}`;
+        console.log(`ver ${image}`)
+      } else {
+        url = "No hay imagen para mostar: ";
+        artistResult.innerText = `${url}`;
+      }
+      console.log(url)
+      artistResult.innerText += element.name;
+      artistList.appendChild(artistResult);
+      artistResult.appendChild(image);
+    });
   })
+  getArtist.value = "";
+  artistList.innerHTML = "";
 })
-*/
