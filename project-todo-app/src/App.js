dcc34e5efde1845090f6de7b3917c2f3edf7262f
Laura Mejía Celis <lauram2celis@gmail.com>
2022-07-11 17:27:14 -0500

    starting project + routes

--- project-todo-app/src/App.js
+++ project-todo-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import styles from './App.module.css';
+import Login from './Login';
+import Register from './Register';
+import Settings from './Setting';
+import Todos from './Todos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.mainContainer}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/todos" element={<Todos />} />
+      </Routes>
     </div>
   );
 }
---
+++ project-todo-app/src/Todos.js
@@ -0,0 +1,9 @@
+function Todos() {
+  return (
+    <div>
+      <h1>My TODOS</h1>
+    </div>
+  );
+}
+
+export default Todos;
