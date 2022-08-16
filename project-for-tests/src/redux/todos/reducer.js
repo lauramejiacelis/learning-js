8925f18e9e989e80e4a26d1b2bbfbdaa85f22841
Laura Mejía Celis <lauram2celis@gmail.com>
2022-08-16 17:38:52 -0500

    editTodo reducer

--- project-for-tests/src/redux/todos/reducer.js
+++ project-for-tests/src/redux/todos/reducer.js
@@ -1,6 +1,9 @@
 import { TODOS_ACTION_TYPES } from './actionTypes';
 
-const initialState = [];
+const initialState = {
+  isEditing: false,
+  todos: [],
+};
 
 let id = 0;
 
@@ -9,16 +12,26 @@ export const todosReducer = (state = initialState, action) => {
     case TODOS_ACTION_TYPES.ADD_TODO:
       console.log('created todo', action.payload);
       id++;
-      return [...state, { id, description: action.payload }];
+      return {
+        ...state,
+        isEditing: false,
+        todos: [...state.todos, { id, description: action.payload }],
+      };
     case TODOS_ACTION_TYPES.DELETE_TODO:
       console.log('Delete');
       console.log(action.payload);
-      return state.filter((todo) => todo.id !== action.payload.id);
+      return {
+        isEditing: false,
+        todos: state.todos.filter((todo) => todo.id !== action.payload.id),
+      };
     case TODOS_ACTION_TYPES.EDIT_TODO:
       console.log('Edit');
-      return state.map((todo) =>
-        todo.id === action.payload.id ? action.payload : todo
-      );
+      return {
+        isEditing: true,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id ? action.payload : todo
+        ),
+      };
     default:
       return state;
   }
