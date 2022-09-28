46b706fd01a9dbe5265713f764ecc1f2b6f05dd4
Laura Mejía Celis <lauram2celis@gmail.com>
2022-09-27 20:17:42 -0500

    starting and adding players

--- react/rock-paper-scissors/src/pages/GameRPS/index.js
+++ react/rock-paper-scissors/src/pages/GameRPS/index.js
@@ -1,13 +1,17 @@
 import {
   Box,
-  Center,
+  Input,
   Grid,
   GridItem,
   Image,
   ListItem,
   Text,
   UnorderedList,
+  HStack,
   VStack,
+  Button,
+  Flex,
+  Heading,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { RPS } from '../../constants';
@@ -16,38 +20,77 @@ const GameRPS = () => {
   const [player, setPlayer] = useState([
     {
       id: 'Player 1',
+      name: '',
       score: 0,
     },
     {
       id: 'Player 2',
+      name: '',
       score: 0,
     },
   ]);
+  const [step, setStep] = useState(0)
   const [turn, setTurn] = useState(0);
+  const [flag, setFlag] = useState('none')
+
+  const handleAdd = () => {
+
+  }
+
+  const handlePlay = () => {
+    setFlag('')
+  }
 
   return (
-    <Box px={20} py={10}>
-      <Box bg={'#CC57C7'} h={10} w={10} display={'none'} />
+    <Box px={20} py={10}> 
+      <Box bg={'#CC57C7'} h={10} w={10} display={flag}>
+      </Box>
+
       <Text color={'#CC57C7'} fontSize={'2xl'} as={'b'}>
         Rock, Paper, Scissors
       </Text>
-      <Grid templateColumns={'1fr 2fr 1fr'} gap={'2'}>
+      
+      <VStack>
+        <Input  placeholder='Player 1 Name' focusBorderColor={'#CC57C7'}/>
+        <Button onClick={handleAdd}>Add Player</Button>
+      </VStack>
+
+      <Grid templateColumns={'1fr 2fr 1fr'} gap={'2'} display={flag}>
         <GridItem bg={'#E19BDE'}>
           <VStack py={5}>
+            
             <Text color={'#333333'} fontSize={'2em'} as={'b'}>
-              Player 1
+              {player[0].id}
             </Text>
             <Text color={'#333333'} fontSize={'large'} as={'b'}>
-              Score: 0
+              Score: {player[0].score}
             </Text>
+
             {RPS.map((option) => (
               <GameOption name={option.name} src={option.src} />
             ))}
           </VStack>
         </GridItem>
-        <GridItem bg={'#E19BDE'} />
+
+        <GridItem bg={'#E19BDE'} >
+          <VStack py={'10'}>
+            <Button 
+            h={'2em'} 
+            color={'#333333'} 
+            fontSize={'4em'} 
+            as={'b'} 
+            p={5} 
+            _hover={{
+              bg: '#333333', color: 'white'
+              }} 
+            onClick={handlePlay}>
+                Play
+            </Button>
+          </VStack>
+        </GridItem>
+
         <GridItem bg={'#E19BDE'}>
-          <VStack py={5}>
+          <HStack py={5}>
             <Text color={'#333333'} fontSize={'2em'} as={'b'}>
               Player 2
             </Text>
@@ -57,7 +100,7 @@ const GameRPS = () => {
             {RPS.map((option) => (
               <GameOption name={option.name} src={option.src} />
             ))}
-          </VStack>
+          </HStack>
         </GridItem>
       </Grid>
       <Box p={10}>
