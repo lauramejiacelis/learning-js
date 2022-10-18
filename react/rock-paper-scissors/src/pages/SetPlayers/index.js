4ce6383f2d34d35d240b0b587e4576d4ec03a898
Laura Mejía Celis <lauram2celis@gmail.com>
2022-10-18 17:24:26 -0500

    bug in a validation

--- react/rock-paper-scissors/src/pages/SetPlayers/index.js
+++ react/rock-paper-scissors/src/pages/SetPlayers/index.js
@@ -1,16 +1,22 @@
-import { Box, Button, Container, Text, Select, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Text,
+  Select,
+  Heading,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 import { InputAdd } from '../../components/InputAdd';
 import { redirect, useParams } from 'react-router-dom';
 
-
 const SetPlayers = () => {
-  const {id} = useParams()
+  const { id } = useParams();
   const [selection, setSelection] = useState(2);
   const [playersNumber, setPlayersNumber] = useState(0);
   const [names, setNames] = useState([]);
 
-  console.log(id)
+  console.log(id);
 
   const handleChange = ({ target: { value } }) => {
     setSelection(value);
@@ -22,11 +28,18 @@ const SetPlayers = () => {
     setPlayersNumber(selection);
   };
   console.log(playersNumber);
+  console.log(names.length);
 
   const handleAdd = (userInput) => {
     setNames([...names, userInput]);
   };
   console.log(names);
+
+  console.log(names.length > 0);
+  console.log(names.length === selection);
+  if (names.length > 0 && names.length === selection) {
+    return 'hello';
+  }
 
   return (
     <Container
@@ -37,34 +50,36 @@ const SetPlayers = () => {
       gap={5}
     >
       {playersNumber === 0 ? (
-      <Container
-        maxW="md"
-        p={10}
-        centerContent={true}
-        justifyContent="center"
-        gap={5}>
-        <Text color={'#333333'} fontSize={'large'} as={'b'}>
-        Choose number of players
-        </Text>
-        <Select onChange={handleChange}>
-          <option value={2}>2 players</option>
-          <option value={3}>3 players</option>
-          <option value={4}>4 players</option>
-        </Select>
-        <Button disabled={playersNumber > 0 ? true : false} onClick={handleClick}>
-          Set Players
-        </Button>
-      </Container>) : (
-          <Container>
-            <Text color={'#333333'} fontSize={'large'} as={'b'}>
-              {`You chose ${playersNumber} players`}
-            </Text>
-            <InputAdd onAdd={handleAdd} />
-          </Container>
-          
-        )}
-      
-      {names.length > 0 && names.length === playersNumber ? 'hello' : ""} 
+        <Container
+          maxW="md"
+          p={10}
+          centerContent={true}
+          justifyContent="center"
+          gap={5}
+        >
+          <Text color={'#333333'} fontSize={'large'} as={'b'}>
+            Choose number of players
+          </Text>
+          <Select onChange={handleChange}>
+            <option value={2}>2 players</option>
+            <option value={3}>3 players</option>
+            <option value={4}>4 players</option>
+          </Select>
+          <Button
+            disabled={playersNumber > 0 ? true : false}
+            onClick={handleClick}
+          >
+            Set Players
+          </Button>
+        </Container>
+      ) : (
+        <Container>
+          <Text color={'#333333'} fontSize={'large'} as={'b'}>
+            {`You chose ${playersNumber} players`}
+          </Text>
+          <InputAdd onAdd={handleAdd} />
+        </Container>
+      )}
     </Container>
   );
 };
