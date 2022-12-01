024c61fc7c4908192b22c10ef60d170315050e2d
Laura Mejía Celis <lauram2celis@gmail.com>
2022-12-01 15:55:21 -0500

    starting with register

--- redux/react-redux-firebase-contactmanagement/src/pages/Login.js
+++ redux/react-redux-firebase-contactmanagement/src/pages/Login.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, Link } from 'react-router-dom'
-import { FaUserPlus, FaFacebookF, FaGooglePlusG } from 'react-icons/fa'
+import { FaUserPlus, FaFacebookF, FaGooglePlusG, FaSignInAlt } from 'react-icons/fa'
 import './Login.css'
 
 const Login = () =>{
@@ -19,24 +19,26 @@ const Login = () =>{
 
   return(
     <div>
-      <div id='logreg-forms'>
+      <div className='logreg-form'>
         <form className="form-signin" onSubmit={handleSubmit}>
           <h1 className="h3 mb-3 font-weight-normal" style={{textAlign: 'center'}}> Sign in</h1>
+
           <div className="social-login">
             <button className="btn google-btn social-btn" type="button" onClick={handleGoogleSignIn}>
-              <span>
-                <FaGooglePlusG/>
-                Sing in with Google+
-              </span>
+              <FaGooglePlusG/>
+              <span> Sing in with Google+</span>
             </button>
+
             <button className="btn facebook-btn social-btn" type="button" onClick={handleFBSignIn}>
+              <FaFacebookF/>
               <span>
-                <FaFacebookF/>
                 Sing in with Facebook
               </span>
             </button>
           </div>
+
           <p style={{textAlign: 'center'}}>OR</p>
+
           <input
           type='email'
           id='inputEmail'
@@ -46,6 +48,7 @@ const Login = () =>{
           onChange={handleChange} 
           value={email}
           required/>
+
           <input
           type='password'
           id='inputPassword'
@@ -55,12 +58,16 @@ const Login = () =>{
           onChange={handleChange} 
           value={password}
           required/>
-          <button className="btn btn-secondary btn-block" type="submit"> Sign in </button>
-          <p>Don't have an account yet?</p>
+
+          <button className="btn btn-secondary btn-block" type="submit">
+            <FaSignInAlt/> Sign in </button>
+
+          <p className="no-register">Don't have an account yet?</p>
+
           <Link to='/register'>
             <button className="btn btn-primary btn-block" type="button">
               <FaUserPlus/>
-              Sign Up 
+              <span> Sign Up New Account</span>
             </button>
           </Link>
         </form>
--- redux/react-redux-firebase-contactmanagement/src/pages/Register.js
+++ redux/react-redux-firebase-contactmanagement/src/pages/Register.js
@@ -1,7 +1,78 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory, Link } from 'react-router-dom'
+import { FaUserPlus, FaArrowAltCircleLeft } from 'react-icons/fa'
+import './Register.css'
+
 const Register = () =>{
+
+  const [state, setState] = useState({
+    displayName: '',
+    email: '',
+    password:'',
+    passwordConfirm:''
+  })
+  
+  const {displayName, email, password, passwordConfirm} = state
+  const handleSubmit = () => {}
+  const handleChange = () =>{}
+
   return(
     <div>
-      <h1>Register</h1>
+      <div className='register-form'>
+        <form className="form-signup" onSubmit={handleSubmit}>
+          <h1 className="h3 mb-3 font-weight-normal" style={{textAlign: 'center'}}> Sign up</h1>
+
+          <input
+          type='text'
+          id='displayName'
+          className= 'form-control'
+          placeholder="Full Name"
+          name="displayName"
+          onChange={handleChange} 
+          value={email}
+          required/>
+
+          <input
+          type='email'
+          id='userEmail'
+          className= 'form-control'
+          placeholder="Email Address"
+          name="email"
+          onChange={handleChange} 
+          value={email}
+          required/>
+
+          <input
+          type='password'
+          id='inputPassword'
+          className= 'form-control'
+          placeholder="Password"
+          name="password"
+          onChange={handleChange} 
+          value={password}
+          required/>
+
+          <input
+          type='password'
+          id='inputRePassword'
+          className= 'form-control'
+          placeholder="Confirm Password"
+          name="passwordConfirm"
+          onChange={handleChange} 
+          value={passwordConfirm}
+          required/>
+
+          <button className="btn btn-primary btn-block" type="submit">
+            <FaUserPlus/> Sign up 
+          </button>
+
+          <Link to='/login'>
+            <FaArrowAltCircleLeft/>
+            Back
+          </Link>
+        </form>
+      </div>
     </div>
   )
 }
