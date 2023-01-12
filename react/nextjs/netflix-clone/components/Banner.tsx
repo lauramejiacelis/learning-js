6e5e251cf30f8d145e44fb1ddaf51259d0c5fe5c
Laura Mejía Celis <lauram2celis@gmail.com>
2023-01-12 17:24:13 -0500

    working in banner and typings.d.ts

--- react/nextjs/netflix-clone/components/Banner.tsx
+++ react/nextjs/netflix-clone/components/Banner.tsx
@@ -1,8 +1,24 @@
-const Banner = () => {
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Movie } from "../typings";
+
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+const Banner = ({netflixOriginals}:Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(()=>{
+    setMovie(netflixOriginals[Math.floor(Math.random()*netflixOriginals.length)])
+  },[netflixOriginals])
+
+  console.log(movie)
+
   return (  
     <div>
       <div>
-        
+        <Image src={`${baseUrl}${movie?.backdrop_path || movie?.backdrop_path}`} alt=''/>
       </div>
     </div>
   );
--- react/nextjs/netflix-clone/pages/index.tsx
+++ react/nextjs/netflix-clone/pages/index.tsx
@@ -1,11 +1,29 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import requests from '../utils/requests'
+import { Movie } from '../typings'
 
-const Home = () => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({ netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries, } : Props) => {
+  console.log(netflixOriginals)
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] '>
       <Head>
@@ -15,7 +33,7 @@ const Home = () => {
 
       <Header/>
       <main>
-        <Banner/>
+        <Banner netflixOriginals={netflixOriginals}/>
         {/* Banner */}
         <section>
           { /* Row */ }
