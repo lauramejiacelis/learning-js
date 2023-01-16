2dc468b834bcd0c197e1ca9d77acac59041ecf59
Laura Mejía Celis <lauram2celis@gmail.com>
2023-01-16 17:24:54 -0500

    finishing thumbnails

--- react/nextjs/netflix-clone/pages/index.tsx
+++ react/nextjs/netflix-clone/pages/index.tsx
@@ -26,7 +26,7 @@ const Home = ({ netflixOriginals,
     documentaries, } : Props) => {
   console.log(netflixOriginals)
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] '>
+    <div className='relative h-screen bg-gradient-to-b  lg:h-[140vh] '>
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -35,7 +35,7 @@ const Home = ({ netflixOriginals,
       <Header/>
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals}/>
-        <section>
+        <section className='space-y-24'>
           <Row title='Trending Now' movies={trendingNow}/>
           <Row title='Top Rated' movies={topRated}/>
           <Row title='Action Movies' movies={actionMovies}/>
