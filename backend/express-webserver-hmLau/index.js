be8f9bffcbb721be53462d95bda4449c1d88ec8d
Laura Mejía Celis <lauram2celis@gmail.com>
2023-02-08 16:49:50 -0500

    working on single pokemon page

--- backend/express-webserver-hmLau/index.js
+++ backend/express-webserver-hmLau/index.js
@@ -28,5 +28,28 @@ app.get('/', (req, res) => {
   
 });
 
+app.get('/pokemon/:name', (req, res)=>{
+  const { name } = req.params;
+  
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(response=>{
+      let pokemonInfo = {
+        name: response.data.name,
+        height: response.data.height,
+        weight: response.data.weight,
+        id: response.data.id,
+        image: response.data.sprites.other.dream_world.front_default,
+        moves: response.data.moves.map(({move})=>move.name),
+        types: response.data.types.map(({type})=>type.name)
+      }
+      response.data
+      console.log(pokemonInfo)
+      res.render('pokemon', { name:pokemonInfo.name.toUpperCase(), height: pokemonInfo.height, weight:pokemonInfo.height, id: pokemonInfo.id, image:pokemonInfo.image, moves: pokemonInfo.moves, types:pokemonInfo.types });
+    })
+    
+    .catch((err) => console.log(err));
+
+})
+
 app.listen(8000, () => console.log("Server listening on port 8000"));
 
