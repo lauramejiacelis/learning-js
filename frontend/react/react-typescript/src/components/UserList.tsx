47b781fcb28b9cc333b8a3dea0c409ede11d1249
Laura Mejía Celis <lauram2celis@gmail.com>
2023-02-17 16:27:01 -0500

    working on list rendering

--- frontend/react/react-typescript/src/components/UserList.tsx
+++ frontend/react/react-typescript/src/components/UserList.tsx
@@ -41,8 +41,45 @@ const UserList:React.FC<IProps> = ()=>{
 
   return(
     <React.Fragment>
-      <pre>{JSON.stringify(state.users)}</pre>
-      <h3>UserList</h3>
+      {/* <pre>{JSON.stringify(state.users)}</pre> */}
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <p className="h3">User List</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <table className="table table-striped text-center table-hover">
+              <thead  className="bg-dark text-white">
+                <tr>
+                  <th>SNO</th>
+                  <th>NAME</th>
+                  <th>AGE</th>
+                  <th>DESIGNATION</th>
+                  <th>COMPANY</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  state.users.length > 0 &&
+                  state.users.map((user)=>{
+                    return (
+                      <tr key={user.sno}>
+                        <td>{user.sno}</td>
+                        <td>{user.name}</td>
+                        <td>{user.age}</td>
+                        <td>{user.designation}</td>
+                        <td>{user.company}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   )
 }
