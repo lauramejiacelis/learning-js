3fde5352f3d18c963dbf9d9f686bc8be7fc8d440
Laura <lauramejiacelis@LAPTOP-8BUF1526>
2023-03-13 19:21:36 -0500

    half homework

--- backend/sequelize-example/src/routes/courseRoutes.js
+++ backend/sequelize-example/src/routes/courseRoutes.js
@@ -1,1 +1,25 @@
 // Implement course get all, get by id, add
+const { Course } = require("../db/models")
+const { Router } = require("express")
+
+const router = Router()
+
+router.get("/", (req, res) => {
+  Course.findAll().then((courses) => {
+    res.send(courses);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Course.findOne({ where: {id}}).then((course)=> res.send(course))
+})
+
+router.post("/", (req, res)=> {
+  const { name } = req.body;
+  Course.create({name}).then((course)=>{
+    res.send(course)
+  })
+})
+
+module.exports = router;
